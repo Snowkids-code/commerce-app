dd893349b92ebd3aade310c18564e4410845a224
Snowkids-code <oumaj47@gmail.com>
2022-07-05 17:23:12 +0300

    add footer component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 import Navbar from "../components/navbar";
 import Newslaetter from "../components/Newslaetter";
 import Products from "../components/Products";
@@ -15,6 +16,7 @@ function Home() {
       <Categories />
       <Products />
       <Newslaetter />
+      <Footer/>
     </div>
   );
 }
