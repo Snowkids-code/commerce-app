3ccca6eb520f2f17e0df62cf3e2ca3b90d62f53f
Snowkids-code <oumaj47@gmail.com>
2022-07-05 17:53:36 +0300

    add react router dom dependancy for app paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import ProductList from "./pages/product-list";
 
 const App = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="shop" element={<ProductList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
